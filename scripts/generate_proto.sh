d6ede156b024c462940018461cfb9bb08919e2e2
Daniel Kohlsdorf <dkohlsdorf@gmail.com>
2023-07-27 19:13:13 +0200

    working on server

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -3,9 +3,13 @@
 rm -rf server/gen
 mkdir server/gen
 
-./build/_deps/grpc-build/third_party/protobuf/protoc -I=protos/ --cpp_out=server/gen/ protos/indexing.proto
-./build/_deps/grpc-build/third_party/protobuf/protoc -I=protos/ --grpc_out=server/gen/ --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin protos/indexing.proto
 
+# OLD WAY
+# ./build/_deps/grpc-build/third_party/protobuf/protoc -I=protos/ --cpp_out=server/gen/ protos/indexing.proto
+# ./build/_deps/grpc-build/third_party/protobuf/protoc -I=protos/ --grpc_out=server/gen/ --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin protos/indexing.proto
+
+protoc -I=protos/ --cpp_out=server/gen/ protos/indexing.proto
+protoc -I=protos/ --grpc_out=server/gen/ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin protos/indexing.proto
 
 rm -rf scripts/gen
 mkdir scripts/gen
--- scripts/replay_keogh.py
+++ scripts/replay_keogh.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('gen')
 
-
+import random
 import numpy as np
 import grpc
 import indexing_pb2
@@ -11,7 +11,9 @@ DATA = "../data/gunpoint.tsv"
 
 def keogh():
     x = np.genfromtxt(DATA)    
-    return x[:, 1:]
+    vectors = x[:, 1:]
+    labels = x[:, 0]
+    return labels, vectors
 
 def timeseries(ts):
     if len(ts.shape) == 1:
@@ -23,7 +25,7 @@ def timeseries(ts):
     return indexing_pb2.TimeSeries(ts=repeated, dim=dim, length=length)
 
 def run():
-    X = keogh()
+    y, X = keogh()
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = indexing_pb2_grpc.TimeSeriesServiceStub(channel)
         for i in range(0, len(X)):
@@ -32,13 +34,22 @@ def run():
             print(f"Insert Response: status = {response.status} id = {response.ts_id}")
         request = indexing_pb2.ReindexingRequest(n_samples = 150)
         response = stub.reindex(request)
-        print(f"Reindexing Response: status = {response.status} id = {response.ts_id}")
-        query = timeseries(X[0])
-        response = stub.query(query)
-        print(f"Query Response: status = {response.status}")
-        for neighbor in response.ids:
-            print(f"\t ... neighbor: {neighbor}")
         response = stub.save(indexing_pb2.SaveIndexRequest(name = "test_save"))
+        response = stub.load(indexing_pb2.LoadIndexRequest(name = "test_save"))
+        print(f"Reindexing Response: status = {response.status} id = {response.ts_id}")
+        for i in range(0, 100):
+            qid = random.randint(0, len(y) - 1)
+            query = timeseries(X[qid])
+            response = stub.query(query)
+            print(f"Query Response: status = {response.status}")
+            n = len(response.ids)
+            c = 0
+            for neighbor in response.ids:
+                c += 1.0 if y[neighbor] == y[qid] else 0
+            p = c / n
+            print(f"p(correct) = {p}")
+
+
         
 if __name__ == "__main__":
     run()
